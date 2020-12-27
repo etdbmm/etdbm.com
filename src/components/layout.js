13dbc745fac20a91df632f8fa9e3b86ce5e2bedb
Daniel Soares <daniel.soares@me.com>
2020-12-27 02:36:26 -0300

    feat: add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import Footer from './footer'
+
 import '../theme/theme-base.css'
 import 'typeface-quicksand'
 
 const Layout = ({ children }) => (
   <>
     <main>{children}</main>
+    <Footer />
   </>
 )
 
