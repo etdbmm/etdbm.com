e3bf2f1a3a9e17564b17cda503e84adcc5e39c50
Daniel Soares <daniel.soares@me.com>
2020-12-27 12:39:29 -0300

    feat: add projects (#5)

---
+++ src/hooks/use-packing-design.js
@@ -0,0 +1,48 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+export default function usePackingDesign() {
+  const {
+    projects: { edges }
+  } = useStaticQuery(graphql`
+    query {
+      projects: allMarkdownRemark(
+        filter: {
+          frontmatter: {
+            key: { eq: "project-item" }
+            role: { eq: "embalagem" }
+          }
+        }
+        limit: 9
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              client
+              content {
+                childImageSharp {
+                  fluid(maxWidth: 1040, quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+              date
+              function
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 480, quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+              role
+              text
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+  return edges
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Projects from '../components/projects'
 import Profile from '../components/profile'
+
+import useBranding from '../hooks/use-branding'
+import useGraphicDesign from '../hooks/use-graphic-design'
+import usePackingDesign from '../hooks/use-packing-design'
+import useUX from '../hooks/use-ux'
 import useProfile from '../hooks/use-profile'
 
 const IndexPage = () => (
@@ -9,6 +15,12 @@ const IndexPage = () => (
     <div className="container">
       <h1>Hello World</h1>
     </div>
+    <Projects
+      brandingData={useBranding()}
+      graphicDesignData={useGraphicDesign()}
+      packingDesignData={usePackingDesign()}
+      uxData={useUX()}
+    />
     <Profile
       image={useProfile().image}
       title={useProfile().title}
