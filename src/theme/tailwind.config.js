85dbe734e8a26524f33dc17beb47951d727a620f
Daniel Soares <daniel.soares@me.com>
2020-12-26 22:36:03 -0300

    style: add theme-base

---
+++ src/theme/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.js'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+          md: '2rem'
+        }
+      }
+    }
+  }
+}
