a79476e51209f0fd83919820282c6af71d69239d
Daniel Soares <daniel.soares@me.com>
2020-12-27 02:24:17 -0300

    style: adjust the theme

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ label, primary, ...props }) => (
   <button
-    className={`py-4 px-5 border border-white text-white font-semibold leading-none rounded-md transition duration-500 ${
+    className={`py-4 px-5 border border-white text-white font-semibold leading-none rounded-xl md:rounded-md transition duration-500 ${
       primary
         ? `bg-pink hover:bg-blue border-pink hover:border-white`
         : `bg-blue hover:border-pink hover:text-pink`
--- src/components/input.js
+++ src/components/input.js
@@ -4,7 +4,7 @@ const baseStyle =
   'block border border-white rounded-md bg-blue text-white w-full p-4 focus:outline-none focus:border-none'
 
 const labelStyle =
-  'absolute -top-3 left-7 pointer-events-none bg-blue text-white px-2'
+  'absolute -top-3 left-6 pointer-events-none bg-blue text-white px-2'
 
 export const Input = ({ label, name, ...props }) => (
   <div className="relative">
