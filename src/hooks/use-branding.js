c4844161f7de885f814276d8e2d867fa649597d0
Daniel Soares <daniel.soares@me.com>
2020-12-27 09:05:19 -0300

    feat: add hooks

---
+++ src/hooks/use-branding.js
@@ -0,0 +1,45 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+export default function useBranding() {
+  const {
+    projects: { edges }
+  } = useStaticQuery(graphql`
+    query {
+      projects: allMarkdownRemark(
+        filter: {
+          frontmatter: { key: { eq: "project-item" }, role: { eq: "branding" } }
+        }
+        limit: 9
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              client
+              content {
+                childImageSharp {
+                  fluid(maxWidth: 1040, quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+              date
+              function
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 480, quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+              role
+              text
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+  return edges
+}
