8515edc2d00e54e89e4e8a63c142220dae379768
Daniel Soares <daniel.soares@me.com>
2020-12-27 00:54:36 -0300

    feat: add theme (#2)

---
+++ src/components/input.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const baseStyle =
+  'block border border-white rounded-md bg-blue text-white w-full p-4 focus:outline-none focus:border-none'
+
+const labelStyle =
+  'absolute -top-3 left-7 pointer-events-none bg-blue text-white px-2'
+
+export const Input = ({ label, name, ...props }) => (
+  <div className="relative">
+    <label className={labelStyle}>{label}</label>
+    <input className={baseStyle} name={name} id={name} {...props} />
+  </div>
+)
+
+export const Textarea = ({ label, name, ...props }) => (
+  <div className="relative">
+    <label className={labelStyle}>{label}</label>
+    <textarea
+      className={`${baseStyle} h-60 resize-none`}
+      name={name}
+      id={name}
+      {...props}
+    />
+  </div>
+)
--- src/theme/tailwind.config.js
+++ src/theme/tailwind.config.js
@@ -8,6 +8,24 @@ module.exports = {
           DEFAULT: '1.5rem',
           md: '2rem'
         }
+      },
+      backgroundColor: {
+        blue: '#0B0043',
+        pink: '#FF9F9F',
+        'pink-hover': '#D05A57'
+      },
+      borderColor: {
+        blue: '#0B0043',
+        pink: '#FF9F9F',
+        'pink-hover': '#D05A57'
+      },
+      fontFamily: {
+        default: 'Quicksand, sans-serif'
+      },
+      textColor: {
+        'cyan-blue': '#00A2A7',
+        gray: '#454545',
+        pink: '#FF9F9F'
       }
     }
   }
